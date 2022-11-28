1f8c61dbf847952912dcd9f20f5ea7f159464f79
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-11-28 13:02:34 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(func){
+    return func();
+
+}
+function returnsANamedFunction(){
+    return (function mike(){
+        
+        
+ 
+
+})
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        return 'Alvin'
+    }
+}
